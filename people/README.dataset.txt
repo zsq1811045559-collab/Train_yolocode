fe1a56a8e0aadf98b928cb29cac3ceaa7a4df2c9
shbiao <zsq1811045559@gmail.com>
2025-10-28 20:03:08 +0800

    Initial clean commit - final version

---
+++ people/README.dataset.txt
@@ -0,0 +1,6 @@
+# crowdhuman > 2024-07-31 9:17am
+https://universe.roboflow.com/objectdetection-1gccs/crowdhuman-dtwxp
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ predict_test.py
@@ -0,0 +1,25 @@
+from charset_normalizer import detect
+from sympy.multipledispatch.dispatcher import source
+
+from ultralytics import YOLO
+
+# 1. 加载模型
+model = YOLO("yolov8n.pt")  # 或 yolov11n.pt 等
+
+# 2. 推理
+results = model.predict(
+    source="ultralytics/assets/bus.jpg",  # 图片或文件夹、视频流
+
+    device=0,
+    save=True
+)
+
+# results=model(source="screen")
+# results=model(source=0)
+
+
+# 3. 显示/保存结果
+# for r in results:
+#     r.show()
+results[0].show()        # 弹出可视化窗口
+#results[0].save("steel_pre.jpg")  # 保存预测图像
