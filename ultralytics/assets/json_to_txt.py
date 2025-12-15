2ea980b1645c4648f9f34ab65b48551d608d448d
UltralyticsAssistant <web@ultralytics.com>
2025-12-15 04:26:33 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/assets/json_to_txt.py
+++ ultralytics/assets/json_to_txt.py
@@ -1,14 +1,15 @@
-import json, os
+import json
 from pathlib import Path
 
 json_dir = Path(r"E:\python project\Yolo_learning\ultralytics-main\ultralytics\assets")
-out_dir = Path(r"E:\python project\Yolo_learning\ultralytics-main\ultralytics\assets\txts"); out_dir.mkdir(exist_ok=True)
+out_dir = Path(r"E:\python project\Yolo_learning\ultralytics-main\ultralytics\assets\txts")
+out_dir.mkdir(exist_ok=True)
 for jpath in json_dir.glob("*.json"):
     data = json.load(open(jpath, encoding="utf-8"))
     W, H = data["imageWidth"], data["imageHeight"]
     out = ""
     for shp in data["shapes"]:
-        pts = [(x/W, y/H) for x, y in shp["points"]]
+        pts = [(x / W, y / H) for x, y in shp["points"]]
         cls = 0  # 或按类别名映射
         out += f"{cls} " + " ".join([f"{x:.6f} {y:.6f}" for x, y in pts]) + "\n"
     open(out_dir / f"{jpath.stem}.txt", "w", encoding="utf-8").write(out)
--- ultralytics/models/sam/build.py
+++ ultralytics/models/sam/build.py
@@ -13,7 +13,14 @@ import torch
 from ultralytics.utils.downloads import attempt_download_asset
 
 from .modules.decoders import MaskDecoder
-from .modules.encoders import FpnNeck, Hiera, ImageEncoder, ImageEncoderViT, MemoryEncoder, PromptEncoder
+from .modules.encoders import (
+    FpnNeck,
+    Hiera,
+    ImageEncoder,
+    ImageEncoderViT,
+    MemoryEncoder,
+    PromptEncoder,
+)
 from .modules.memory_attention import MemoryAttention, MemoryAttentionLayer
 from .modules.sam import SAM2Model, SAMModel
 from .modules.tiny_encoder import TinyViT
@@ -126,8 +133,7 @@ def _build_sam(
     checkpoint=None,
     mobile_sam=False,
 ):
-    """
-    Build a Segment Anything Model (SAM) with specified encoder parameters.
+    """Build a Segment Anything Model (SAM) with specified encoder parameters.
 
     Args:
         encoder_embed_dim (int | list[int]): Embedding dimension for the encoder.
@@ -223,8 +229,7 @@ def _build_sam2(
     encoder_window_spec=[8, 4, 16, 8],
     checkpoint=None,
 ):
-    """
-    Build and return a Segment Anything Model 2 (SAM2) with specified architecture parameters.
+    """Build and return a Segment Anything Model 2 (SAM2) with specified architecture parameters.
 
     Args:
         encoder_embed_dim (int, optional): Embedding dimension for the encoder.
@@ -325,8 +330,7 @@ sam_model_map = {
 
 
 def build_sam(ckpt="sam_b.pt"):
-    """
-    Build and return a Segment Anything Model (SAM) based on the provided checkpoint.
+    """Build and return a Segment Anything Model (SAM) based on the provided checkpoint.
 
     Args:
         ckpt (str | Path, optional): Path to the checkpoint file or name of a pre-defined SAM model.
